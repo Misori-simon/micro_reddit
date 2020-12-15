0c05790f1512e6c2862d90de184c430712e74bf7
Misori-simon <misorisimon@gmail.com>
2020-12-15 16:18:42 +0100

    add post model, and association -add post model with attributes title, body -add association for post to user via user_id -add relation between posts and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :name, :email, uniqueness: true
-  validates :name, :email, :password,  presence: true
-  validates :name, :password, length: {in: 6..10}
+  validates :name, :email, :password, presence: true
+  validates :name, :password, length: { in: 6..10 }
 end
